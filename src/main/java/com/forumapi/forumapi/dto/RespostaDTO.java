9f9a23665121af84da05484c2a2bb50c73656fe4
gmozart <mozart.gleison@gmail.com>
2023-02-02 21:13:37 -0300

    Classe RespostaDTO finalizada.

--- src/main/java/com/forumapi/forumapi/dto/RespostaDTO.java
+++ src/main/java/com/forumapi/forumapi/dto/RespostaDTO.java
@@ -1,12 +1,15 @@
 package com.forumapi.forumapi.dto;
 
+import com.forumapi.forumapi.entity.Resposta;
 import com.forumapi.forumapi.entity.Topico;
 import com.forumapi.forumapi.entity.Usuario;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -20,5 +23,32 @@ public class RespostaDTO {
     private Boolean solucao = false;
 
 
+    public static RespostaDTO of(Resposta resposta){
+        return RespostaDTO.builder()
+                .id(resposta.getId())
+                .mensagem(resposta.getMensagem())
+                .dataCriacao(resposta.getDataCriacao())
+                .autor(resposta.getAutor())
+                .solucao(resposta.getSolucao())
+                .build();
+    }
+
+    public static  Resposta of(RespostaDTO respostaDTO){
+        return Resposta.builder()
+                .id(respostaDTO.getId())
+                .mensagem(respostaDTO.getMensagem())
+                .dataCriacao(respostaDTO.getDataCriacao())
+                .autor(respostaDTO.getAutor())
+                .solucao(respostaDTO.getSolucao())
+                .build();
+    }
+
+    public Optional<RespostaDTO> of(Optional<Resposta> resposta){
+        return  resposta.stream().map(RespostaDTO::of).findAny();
+    }
+
+    public List<RespostaDTO> of(List<Resposta> respostas){
+        return respostas.stream().map(RespostaDTO::of).collect(Collectors.toList());
+    }
 
 }
